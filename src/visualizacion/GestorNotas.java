190876d2418cf4ecfb90cae47dd61af8e067e9c4
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 18:39:08 +0100

    En la clase GestorNotas, añadi la extension JFrame

--- src/visualizacion/GestorNotas.java
+++ src/visualizacion/GestorNotas.java
@@ -5,7 +5,7 @@ import modelo.Usuario;
 import javax.swing.*;
 import java.util.ArrayList;
 
-public class GestorNotas {
+public class GestorNotas extends JFrame {
     private JTextField txtTitulo;
     private JTextArea txtContenido;
 
