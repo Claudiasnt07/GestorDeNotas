8b74dbcae046aa8e095e46a97d322ee6e39d818a
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 15:35:24 +0100

    Importe las clases necesarias para esta parte el programa

---
+++ src/visualizacion/Registro.java
@@ -0,0 +1,11 @@
+package visualizacion;
+
+import modelo.Usuario;
+import persistencia.GestorFicheros;
+import util.HashUtil;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Registro {
+    
+}
