c7125afe6bd4c6a58b901d0fe189fd1e09f132d0
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 15:05:05 +0100

    Cree el método para guaradar la lista de usuarios en el archivo

--- src/persistencia/GestorFicheros.java
+++ src/persistencia/GestorFicheros.java
@@ -7,4 +7,16 @@ import modelo.Usuario;
 public class GestorFicheros {
 
     private static final String ARCHIVO_USUARIOS = "usuarios.txt";
+
+    public static void guardarUsuarios(
+            ArrayList<Usuario> usuarios) {
+        try (ObjectOutputStream oos =
+                    new ObjectOutputStream(
+                            new FileOutputStream(
+                                    ARCHIVO_USUARIOS))) {
+            oos.writeObject(usuarios);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
