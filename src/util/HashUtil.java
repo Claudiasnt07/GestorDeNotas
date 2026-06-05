b49b3306b1798870b254ca0118c1f82692e55288
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 13:54:28 +0100

    Cree el método estatico con el hash y el SHA-256

--- src/util/HashUtil.java
+++ src/util/HashUtil.java
@@ -3,5 +3,18 @@ package util;
 import java.security.MessageDigest;
 
 public class HashUtil {
-    
+    public static String hash(String texto) {
+        try {
+            MessageDigest md =
+                    MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(texto.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
