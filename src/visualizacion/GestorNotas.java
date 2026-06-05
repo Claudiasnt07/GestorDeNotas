2bae904110c66b8cb88ec1e593e9ae13606bae9e
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 18:30:57 +0100

    Cree el metodo que crea como se va a ver la pagina

--- src/visualizacion/GestorNotas.java
+++ src/visualizacion/GestorNotas.java
@@ -26,4 +26,53 @@ public class GestorNotas {
 
         setVisible(true);
     }
+
+    private void initComponents() {
+        JPanel panel = new JPanel();
+
+        panel.add(new JLabel("Título:"));
+
+        txtTitulo = new JTextField(20);
+        panel.add(txtTitulo);
+
+        panel.add(new JLabel("Contenido:"));
+
+        txtContenido = new JTextArea(5, 20);
+        panel.add(new JScrollPane(txtContenido));
+
+        modeloLista = new DefaultListModel<>();
+        listaNotas = new JList<>(modeloLista);
+
+        for (Nota nota : notas) {
+            modeloLista.addElement(nota);
+        }
+
+        panel.add(new JScrollPane(listaNotas));
+
+        JButton btnCrear = new JButton("Crear");
+        JButton btnEditar = new JButton("Editar");
+        JButton btnEliminar = new JButton("Eliminar");
+        JButton btnLimpiar = new JButton("Limpiar");
+        JButton btnBorrarTodo = new JButton("Borrar Todo");
+
+        panel.add(btnCrear);
+        panel.add(btnEditar);
+        panel.add(btnEliminar);
+        panel.add(btnLimpiar);
+        panel.add(btnBorrarTodo);
+
+        add(panel);
+
+        btnCrear.addActionListener(e -> crearNota());
+
+        btnEditar.addActionListener(e -> editarNota());
+
+        btnEliminar.addActionListener(e -> eliminarNota());
+
+        btnLimpiar.addActionListener(e -> limpiarCampos());
+
+        btnBorrarTodo.addActionListener(e -> borrarTodas());
+
+        listaNotas.addListSelectionListener(e -> mostrarNota());
+    }
 }
