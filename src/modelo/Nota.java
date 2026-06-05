027349f1b8675ca88265369206f95531f856b4b3
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 13:26:54 +0100

    Cree el método para poder modificar el contenido cuando queramos

--- src/modelo/Nota.java
+++ src/modelo/Nota.java
@@ -20,4 +20,8 @@ public class Nota {
     public String getContenido() {
         return contenido;
     }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
 }
