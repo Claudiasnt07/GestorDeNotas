775031d37d5c1f3b3171c3d533dc3986227cc488
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 13:22:02 +0100

    Se crea una nota con los valores que le demos a los anteriores atributos

--- src/modelo/Nota.java
+++ src/modelo/Nota.java
@@ -3,4 +3,9 @@ package modelo;
 public class Nota {
     private String titulo;
     private String contenido;
+
+    public Nota(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
 }
