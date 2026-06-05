3f7ff31bb48941fa10cc87e03ac4255f14e27852
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 13:35:59 +0100

    Cree el método para que al iniciar el programa salga la pantalla de inicio de sesion con el usuario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 import visualizacion.Login;
 
 public class Main {
-    
+ 
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new Login().setVisible(true);
+        });
+    }
 }
